1fdbe5fa7c83b967813041bacd013f109a12142b
stevenhales <steven.hales@hotmail.co.uk>
2023-06-22 08:57:15 +0100

    ref changed

--- src/Today/index.js
+++ src/Today/index.js
@@ -1,11 +1,12 @@
-import React, {PureComponent} from 'react';
-import PropTypes from 'prop-types';
-import classNames from 'classnames';
-import styles from './Today.scss';
+import React, { PureComponent } from "react";
+import PropTypes from "prop-types";
+import classNames from "classnames";
+import styles from "./Today.scss";
 
 export const DIRECTION_UP = 1;
 export const DIRECTION_DOWN = -1;
-const CHEVRON = 'M256,298.3L256,298.3L256,298.3l174.2-167.2c4.3-4.2,11.4-4.1,15.8,0.2l30.6,29.9c4.4,4.3,4.5,11.3,0.2,15.5L264.1,380.9 c-2.2,2.2-5.2,3.2-8.1,3c-3,0.1-5.9-0.9-8.1-3L35.2,176.7c-4.3-4.2-4.2-11.2,0.2-15.5L66,131.3c4.4-4.3,11.5-4.4,15.8-0.2L256,298.3 z';
+const CHEVRON =
+  "M256,298.3L256,298.3L256,298.3l174.2-167.2c4.3-4.2,11.4-4.1,15.8,0.2l30.6,29.9c4.4,4.3,4.5,11.3,0.2,15.5L264.1,380.9 c-2.2,2.2-5.2,3.2-8.1,3c-3,0.1-5.9-0.9-8.1-3L35.2,176.7c-4.3-4.2-4.2-11.2,0.2-15.5L66,131.3c4.4-4.3,11.5-4.4,15.8-0.2L256,298.3 z";
 
 export default class Today extends PureComponent {
   static propTypes = {
@@ -16,13 +17,13 @@ export default class Today extends PureComponent {
   };
 
   scrollToToday = () => {
-    let {scrollToDate} = this.props;
+    let { scrollToDate } = this.props;
 
     scrollToDate(new Date(), -40, true);
   };
 
   render() {
-    let {todayLabel, show, theme} = this.props;
+    let { todayLabel, show, theme } = this.props;
     return (
       <div
         className={classNames(styles.root, {
@@ -35,7 +36,7 @@ export default class Today extends PureComponent {
           color: theme.floatingNav.color,
         }}
         onClick={this.scrollToToday}
-        ref="node"
+        ref={(el) => (this.node = el)}
       >
         {todayLabel}
         <svg
